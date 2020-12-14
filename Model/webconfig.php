491531abaad16dae3fab7e2f4110140fc13018ec
Luis VP <luisvalles92@hotmail.com>
2020-12-14 15:27:44 +0100

    Commit Jelastic lines

--- Model/webconfig.php
+++ Model/webconfig.php
@@ -1,4 +1,4 @@
 <?php
-define('DNS', 'mysql:dbname=biblioteca;host=localhost');
+define('DNS', 'mysql:dbname=biblioteca;host=node29291-env-6120084.es-1.axarnet.cloud');
 define('USERNAME', 'root');
-define('PASSWORD', '');
+define('PASSWORD', 'PXBpan94442');
--- README.md
+++ README.md
@@ -4,4 +4,4 @@ PDO - MVC - Encriptación sha512 - PHP - MySQL
 
 Despliegue sobre Jelastic:
 
-
+http://env-6120084.es-1.axarnet.cloud
